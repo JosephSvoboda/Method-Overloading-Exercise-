996297d27160ca01a9a7cb21403962516cd332a0
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-10-25 10:32:31 -0400

    made an AddMethod class and stored all methods in there.

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(AddMethod.Add(1, 2, true));
+            
         }
     }
 }
